064cc6879b0f63d6533fa9289d35b1b7433ca793
itayge@wincs.cs.bgu.ac.il <itayge@post.bgu.ac.il>
2022-02-02 11:47:38 +0200

    init

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-
-
+import sqlite3
+import os
+import importlib
+import atexit
 
 
 if __name__ == '__main__':
     print('PyCharm')
+
+def main():
+    connections = sqlite3.connect('myDB.db')
+
+    connections.execute("""
+    CREATE TABLE suppliers (
+        id INTEGER PRIMARY KEY,
+        name STRING NOT NULL
+    )""")
+
+    connections.execute("""
+    CREATE TABLE hats (
+        id INTEGER PRIMARY KEY,
+        topping STRING NOT NULL,
+    supplier INTEGER,
+        FOREIGN KEY(supplier) REFERENCES suppliers(id),
+        quantity INTEGER NOT NULL
+    )""")
+
+    connections.execute("""
+        CREATE TABLE orders (
+            is INTEGER PRIMARY KEY,
+            location STRING NOT NULL,
+        )hat""")
 
